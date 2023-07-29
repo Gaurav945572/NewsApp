b9fc30d1c9d6a0d0b620f315880439d3d8975eac
Gaurav945572 <gauravoppo9455@gmail.com>
2023-07-29 19:51:17 +0530

    News Hub
    Added HTML, CSS, JS and images for the News app

---
+++ script.js
@@ -0,0 +1,170 @@
+const API_KEY = "601a9402859e49fcba9ca1811d60ab1e";
+const url = "https://newsapi.org/v2/everything?q=";
+
+window.addEventListener("load", () => fetchNews("India"));
+
+function reload() {
+    window.location.reload();
+}
+
+async function fetchNews(query) {
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    const data = await res.json();
+    bindData(data.articles);
+}
+
+function bindData(articles) {
+    const cardsContainer = document.getElementById("cards-container");
+    const newsCardTemplate = document.getElementById("template-news-card");
+
+    cardsContainer.innerHTML = "";
+
+    articles.forEach((article) => {
+        if (!article.urlToImage) return;
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardClone, article);
+        cardsContainer.appendChild(cardClone);
+    });
+}
+let a;
+p = ()=>{
+    a= new Date();
+    document.querySelector('.time').innerHTML =a.getHours() + ":" + a.getMinutes() +":" +a.getSeconds()}
+setInterval( p,1000);
+
+
+
+function fillDataInCard(cardClone, article) {
+    const newsImg = cardClone.querySelector("#news-img");
+    const newsTitle = cardClone.querySelector("#news-title");
+    const newsSource = cardClone.querySelector("#news-source");
+    const newsDesc = cardClone.querySelector("#news-desc");
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
+
+    const date = new Date(article.publishedAt).toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta",
+    });
+
+    newsSource.innerHTML = `${article.source.name} · ${date}`;
+
+    cardClone.firstElementChild.addEventListener("click", () => {
+        window.open(article.url, "_blank");
+    });
+}
+
+let curSelectedNav = null;
+function onNavItemClick(id) {
+    fetchNews(id);
+    const navItem = document.getElementById(id);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav = navItem;
+    curSelectedNav.classList.add("active");
+}
+
+const searchButton = document.getElementById("search-button");
+const searchText = document.getElementById("search-text");
+
+searchButton.addEventListener("click", () => {
+    const query = searchText.value;
+    if (!query) return;
+    fetchNews(query);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav = null;
+});
+
+
+
+const openNote=document.querySelector("#openNote");
+openNote.addEventListener('click',()=>{
+    const notes=document.querySelector('#note-container')
+    notes.style.display='flex' 
+})
+const closeNote=document.querySelector('#close-note');
+closeNote.addEventListener('click',()=>{
+    const notes=document.querySelector('#note-container')
+    notes.style.display='none'
+
+})
+showNotes();
+
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener('click', function () {
+    let addTxt = document.getElementById('addTxt');
+    let title=document.getElementById('title');
+    let notes = localStorage.getItem('notes');
+
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+
+    notesObj.push({title:title.value,description:addTxt.value});
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    addTxt.value = '';
+    showNotes();
+});
+
+function showNotes() {
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+
+    let html = "";
+    notesObj.forEach(function (element, index) {
+        html += `
+        <div class="noteCard">
+        <div class="card-body">
+          <h5 class="card-title">${element.title}</h5>
+          <p class="card-text">${element.description}</p>
+          <button href="#" class="delete-btn"  id="${index}" onclick="deleteNote(this.id)">Delete Note</button>
+        </div>
+      </div>`;
+    });
+
+    let notesElm = document.getElementById('notes');
+    if (notesObj.length != 0) {
+        notesElm.innerHTML = html;
+    }
+    else {
+        notesElm.innerHTML = `No, notes! Click on Add Note Section to add Notes!`;
+    }
+}
+
+function deleteNote(index) {
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.splice(index, 1);
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    showNotes();
+}
+
+
+let search = document.getElementById('searchTxt');
+search.addEventListener('input', function () {
+    let inputVal = search.value.toLowerCase;
+
+    let noteCards = document.getElementsByClassName('noteCard');
+    Array.from(noteCards).forEach(function (element) {
+        let cardTxt = element.getElementsByTagName('p')[0].innerText;
+        if (cardTxt.includes(inputVal)) {
+            element.style.display = "block";
+        }
+        else {
+            element.style.display = "none";
+        }
+    });
+});
